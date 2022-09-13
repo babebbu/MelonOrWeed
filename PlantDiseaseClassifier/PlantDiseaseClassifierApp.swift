654037cdc87dab3b6e71271de1db74a5a47d82b5
Natthasak Vechprasit <9090871+babebbu@users.noreply.github.com>
2022-09-14 03:22:16 +0700

    Initial Commit

---
+++ PlantDiseaseClassifier/PlantDiseaseClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantDiseaseClassifierApp.swift
+//  PlantDiseaseClassifier
+//
+//  Created by Babebbu on 14/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct PlantDiseaseClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
